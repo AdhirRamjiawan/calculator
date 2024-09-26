32bfe6e05b138eb408e401457aec2f70c4260d50
Adhir Ramjiawan <adhirramjiawan0@gmail.com>
2024-09-26 19:17:01 +0200

    some basic test cases

--- src/Calculator.cs
+++ src/Calculator.cs
@@ -4,7 +4,20 @@ namespace calculator;
 
 public class Calculator : ICalculator
 {
+    public enum State
+    {
+        Cleared = 0,
+        Dirty,
+        Error
+    }
+
+    private State _state;
     private decimal _result = 0;
+
+    public Calculator()
+    {
+        _state = State.Cleared;
+    }
 
     public decimal Add(decimal number)
     {
@@ -15,28 +28,51 @@ public class Calculator : ICalculator
     public void Clear()
     {
         _result = 0;
+        _state = State.Cleared;
     }
 
     public decimal Divide(decimal number)
     {
-        throw new System.NotImplementedException();
+        if (_result == 0)
+        {
+            _state = State.Error;
+            return _result;
+        }
+
+        _result /= number;
+        return _result;
     }
 
     public decimal GetResult() => _result;
 
     public decimal Multiply(decimal number)
     {
-        throw new System.NotImplementedException();
+        if (_state == State.Cleared)
+        {
+            _result = number;
+            _state = State.Dirty;
+            return _result;
+        }
+
+        _result *= number;
+        return _result;
     }
 
     public decimal Subtract(decimal number)
     {
-        throw new System.NotImplementedException();
+        _result -= number;
+        return _result;
+    }
+
+    public void Initialise(decimal number)
+    {
+        _result = number;
     }
 }
 
 public interface ICalculator
 {
+    public void Initialise(decimal number);
     public decimal GetResult();
     public void Clear();
     public decimal Add(decimal number);
--- tests/CalculatorTests.cs
+++ tests/CalculatorTests.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using calculator;
 
 namespace tests;
@@ -39,6 +38,98 @@ public class CalculatorTests
         }
 
         // Assert
-        Assert.Equal(15, _calculator.GetResult());
+        Assert.Equal(3 + 5 + 7, _calculator.GetResult());
+    }
+
+    [Fact]
+    public void Subtract_WhenGivenANumber_ShouldSubtractFromAccumulation()
+    {
+        // Arrange
+        var number = 1;
+
+        // Act
+        var result = _calculator.Subtract(number);
+
+        // Assert
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void Subtract_WhenCalledMultipleTimes_ShouldSubtractFromAccumulation()
+    {
+        // Arrange
+        var numbers = new decimal[]{ 3, 5, 7 };
+
+        // Act
+
+        foreach (var num in numbers)
+        {
+            _calculator.Subtract(num);
+        }
+
+        // Assert
+        Assert.Equal(-3 - 5 - 7, _calculator.GetResult());
+    }
+
+    [Fact]
+    public void Multiply_WhenGivenANumber_ShouldMultiplyToAccumulation()
+    {
+        // Arrange
+        var number = 1;
+
+        // Act
+        var result = _calculator.Multiply(number);
+
+        // Assert
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void Multiply_WhenCalledMultipleTimes_ShouldMultiplyToAccumulation()
+    {
+        // Arrange
+        var numbers = new decimal[]{ 3, 5, 1 };
+
+        // Act
+
+        foreach (var num in numbers)
+        {
+            _calculator.Multiply(num);
+        }
+
+        // Assert
+        Assert.Equal(3 * 5 * 1, _calculator.GetResult());
+    }
+
+    [Fact]
+    public void Divide_WhenGivenANumber_ShouldDivideFromAccumulation()
+    {
+        // Arrange
+        var number = 1;
+        _calculator.Initialise(1);
+
+        // Act
+        var result = _calculator.Divide(number);
+
+        // Assert
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void Divide_WhenCalledMultipleTimes_ShouldDivideFromAccumulation()
+    {
+        // Arrange
+        var numbers = new decimal[]{ 3m, 5, 1 };
+        _calculator.Initialise(1);
+
+        // Act
+
+        foreach (var num in numbers)
+        {
+            _calculator.Divide(num);
+        }
+
+        // Assert
+        Assert.Equal(0.0666666666666666666666666667m, _calculator.GetResult());
     }
 }
